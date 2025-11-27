7a50968583efbfc4401103f808b04d85f501971c
Hawkai10 <arvindkedia10@gmail.com>
2025-11-27 12:59:26 +0530

    rss feeds list add

---
+++ rss_feeds_config.py
@@ -0,0 +1,516 @@
+# RSS Feeds Configuration
+
+# This file contains all RSS feed URLs, sources, and bias information
+
+RSS_FEEDS = [
+    {
+        "url": "http://timesofindia.indiatimes.com/rssfeeds/-2128936835.cms",
+        "source": "Times of India",
+        "bias": "Right"
+    },
+    {
+        "url": "http://timesofindia.indiatimes.com/rssfeeds/4719148.cms",
+        "source": "Times of India",
+        "bias": "Center"
+    },
+    {
+        "url": "http://timesofindia.indiatimes.com/rssfeeds/296589292.cms",
+        "source": "Times of India",
+        "bias": "Left"
+    },
+    {
+        "url": "http://timesofindia.indiatimes.com/rssfeeds/1898055.cms",
+        "source": "Times of India",
+        "bias": "Left"
+    },
+    {
+        "url": "http://timesofindia.indiatimes.com/rssfeedstopstories.cms",
+        "source": "Times of India",
+        "bias": "Left"
+    },
+    {
+        "url": "https://www.thehindu.com/news/national/feeder/default.rss",
+        "source": "The Hindu",
+        "bias": "Right"
+    },
+    {
+        "url": "https://www.thehindu.com/sport/feeder/default.rss",
+        "source": "The Hindu",
+        "bias": "Right"
+    },
+    {
+        "url": "https://www.thehindu.com/news/international/feeder/default.rss",
+        "source": "The Hindu",
+        "bias": "Right"
+    },
+    {
+        "url": "https://www.thehindu.com/business/feeder/default.rss",
+        "source": "The Hindu",
+        "bias": "Left"
+    },
+    {
+        "url": "https://www.thehindu.com/feeder/default.rss",
+        "source": "The Hindu",
+        "bias": "Center"
+    },
+    {
+        "url": "https://www.hindustantimes.com/feeds/rss/india-news/rssfeed.xml",
+        "source": "Hindustan Times",
+        "bias": "Left"
+    },
+    {
+        "url": "https://www.hindustantimes.com/feeds/rss/sports/rssfeed.xml",
+        "source": "Hindustan Times",
+        "bias": "Center"
+    },
+    {
+        "url": "https://www.hindustantimes.com/feeds/rss/world-news/rssfeed.xml",
+        "source": "Hindustan Times",
+        "bias": "Right"
+    },
+    {
+        "url": "https://www.hindustantimes.com/feeds/rss/business/rssfeed.xml",
+        "source": "Hindustan Times",
+        "bias": "Left"
+    },
+    {
+        "url": "https://www.hindustantimes.com/feeds/rss/trending/rssfeed.xml",
+        "source": "Hindustan Times",
+        "bias": "Right"
+    },
+    {
+        "url": "https://indianexpress.com/section/india/feed/",
+        "source": "Indian Express",
+        "bias": "Center"
+    },
+    {
+        "url": "https://indianexpress.com/section/sports/feed/",
+        "source": "Indian Express",
+        "bias": "Right"
+    },
+    {
+        "url": "https://indianexpress.com/section/world/feed/",
+        "source": "Indian Express",
+        "bias": "Center"
+    },
+    {
+        "url": "https://indianexpress.com/section/business/feed/",
+        "source": "Indian Express",
+        "bias": "Left"
+    },
+    {
+        "url": "https://indianexpress.com/section/politics/feed/",
+        "source": "Indian Express",
+        "bias": "Left"
+    },
+    {
+        "url": "https://indiapioneer.in/category/news/india/feed/",
+        "source": "India Pioneer",
+        "bias": "Center"
+    },
+    {
+        "url": "https://indiapioneer.in/category/news/sports/feed",
+        "source": "India Pioneer",
+        "bias": "Center"
+    },
+    {
+        "url": "https://indiapioneer.in/category/news/business/feed",
+        "source": "India Pioneer",
+        "bias": "Left"
+    },
+    {
+        "url": "https://indiapioneer.in/category/news/trending/feed",
+        "source": "India Pioneer",
+        "bias": "Right"
+    },
+    {
+        "url": "https://indiapioneer.in/category/news/politics/feed",
+        "source": "India Pioneer",
+        "bias": "Left"
+    },
+    {
+        "url": "https://www.livemint.com/rss/markets",
+        "source": "Livemint",
+        "bias": "Right"
+    },
+    {
+        "url": "https://www.livemint.com/rss/sports",
+        "source": "Livemint",
+        "bias": "Left"
+    },
+    {
+        "url": "https://www.livemint.com/rss/industry",
+        "source": "Livemint",
+        "bias": "Center"
+    },
+    {
+        "url": "https://www.livemint.com/rss/politics",
+        "source": "Livemint",
+        "bias": "Center"
+    },
+    {
+        "url": "https://www.livemint.com/rss/news",
+        "source": "Livemint",
+        "bias": "Left"
+    },
+    {
+        "url": "https://www.livemint.com/rss/opinion",
+        "source": "Livemint",
+        "bias": "Center"
+    },
+    {
+        "url": "https://feeds.feedburner.com/ndtvnews-top-stories",
+        "source": "NDTV",
+        "bias": "Center"
+    },
+    {
+        "url": "https://feeds.feedburner.com/ndtvnews-india-news",
+        "source": "NDTV",
+        "bias": "Center"
+    },
+    {
+        "url": "https://feeds.feedburner.com/ndtvnews-latest",
+        "source": "NDTV",
+        "bias": "Left"
+    },
+    {
+        "url": "https://feeds.feedburner.com/ndtvnews-world-news",
+        "source": "NDTV",
+        "bias": "Left"
+    },
+    {
+        "url": "https://feeds.feedburner.com/ndtvsports-latest",
+        "source": "NDTV",
+        "bias": "Left"
+    },
+    {
+        "url": "https://feeds.feedburner.com/ndtvnews-trending-news",
+        "source": "NDTV",
+        "bias": "Right"
+    },
+    {
+        "url": "https://feeds.feedburner.com/ndtvprofit-latest",
+        "source": "NDTV",
+        "bias": "Center"
+    },
+    {
+        "url": "https://www.abplive.com/home/feed",
+        "source": "ABP Live",
+        "bias": "Right"
+    },
+    {
+        "url": "https://www.abplive.com/news/india/feed",
+        "source": "ABP Live",
+        "bias": "Left"
+    },
+    {
+        "url": "https://www.abplive.com/news/world/feed",
+        "source": "ABP Live",
+        "bias": "Right"
+    },
+    {
+        "url": "https://www.abplive.com/sports/feed",
+        "source": "ABP Live",
+        "bias": "Right"
+    },
+    {
+        "url": "https://www.abplive.com/business/feed",
+        "source": "ABP Live",
+        "bias": "Left"
+    },
+    {
+        "url": "https://www.abplive.com/trending/feed",
+        "source": "ABP Live",
+        "bias": "Right"
+    },
+    {
+        "url": "https://www.abplive.com/sports/ipl/feed",
+        "source": "ABP Live",
+        "bias": "Left"
+    },
+    {
+        "url": "https://www.republicworld.com/rss/business.xml",
+        "source": "Republic World",
+        "bias": "Left"
+    },
+    {
+        "url": "https://www.republicworld.com/rss/sports.xml",
+        "source": "Republic World",
+        "bias": "Right"
+    },
+    {
+        "url": "https://www.republicworld.com/rss/politics.xml",
+        "source": "Republic World",
+        "bias": "Left"
+    },
+    {
+        "url": "https://www.republicworld.com/rss/latest-news.xml",
+        "source": "Republic World",
+        "bias": "Center"
+    },
+    {
+        "url": "https://www.republicworld.com/rss/india/politics.xml",
+        "source": "Republic World",
+        "bias": "Left"
+    },
+    {
+        "url": "https://www.republicworld.com/rss/business/markets.xml",
+        "source": "Republic World",
+        "bias": "Center"
+    },
+    {
+        "url": "https://www.republicworld.com/rss/web-stories/economy.xml",
+        "source": "Republic World",
+        "bias": "Center"
+    },
+    {
+        "url": "https://www.republicworld.com/rss/galleries/business.xml",
+        "source": "Republic World",
+        "bias": "Right"
+    },
+    {
+        "url": "https://www.republicworld.com/rss/world-news.xml",
+        "source": "Republic World",
+        "bias": "Left"
+    },
+    {
+        "url": "https://www.republicworld.com/rss/india.xml",
+        "source": "Republic World",
+        "bias": "Left"
+    },
+    {
+        "url": "https://zeenews.india.com/rss/india-national-news.xml",
+        "source": "Zee News",
+        "bias": "Left"
+    },
+    {
+        "url": "https://zeenews.india.com/rss/world-news.xml",
+        "source": "Zee News",
+        "bias": "Left"
+    },
+    {
+        "url": "https://zeenews.india.com/rss/business.xml",
+        "source": "Zee News",
+        "bias": "Center"
+    },
+    {
+        "url": "https://zeenews.india.com/rss/sports-news.xml",
+        "source": "Zee News",
+        "bias": "Left"
+    },
+    {
+        "url": "https://zeenews.india.com/rss/blog-news.xml",
+        "source": "Zee News",
+        "bias": "Right"
+    },
+    {
+        "url": "https://www.aajtak.in/rssfeeds/?id=home",
+        "source": "Aaj Tak",
+        "bias": "Right"
+    },
+    {
+        "url": "https://www.news18.com/commonfeeds/v1/eng/rss/india.xml",
+        "source": "News18",
+        "bias": "Left"
+    },
+    {
+        "url": "https://www.news18.com/commonfeeds/v1/eng/rss/world.xml",
+        "source": "News18",
+        "bias": "Center"
+    },
+    {
+        "url": "https://www.news18.com/commonfeeds/v1/eng/rss/business.xml",
+        "source": "News18",
+        "bias": "Right"
+    },
+    {
+        "url": "https://www.news18.com/commonfeeds/v1/eng/rss/politics.xml",
+        "source": "News18",
+        "bias": "Left"
+    },
+    {
+        "url": "https://www.news18.com/commonfeeds/v1/eng/rss/sports.xml",
+        "source": "News18",
+        "bias": "Left"
+    },
+    {
+        "url": "https://www.news18.com/commonfeeds/v1/eng/rss/viral.xml",
+        "source": "News18",
+        "bias": "Right"
+    },
+    {
+        "url": "https://www.indiatoday.in/rss/home",
+        "source": "India Today",
+        "bias": "Right"
+    },
+    {
+        "url": "https://www.indiatoday.in/rss/1206514",
+        "source": "India Today",
+        "bias": "Right"
+    },
+    {
+        "url": "https://www.indiatoday.in/rss/1206614",
+        "source": "India Today",
+        "bias": "Left"
+    },
+    {
+        "url": "https://www.indiatoday.in/rss/1206509",
+        "source": "India Today",
+        "bias": "Right"
+    },
+    {
+        "url": "https://www.indiatoday.in/rss/1206614",
+        "source": "India Today",
+        "bias": "Right"
+    },
+    {
+        "url": "https://www.indiatoday.in/rss/1206550",
+        "source": "India Today",
+        "bias": "Left"
+    },
+    {
+        "url": "https://www.indiatoday.in/rss/1206513",
+        "source": "India Today",
+        "bias": "Left"
+    },
+    {
+        "url": "https://www.indiatoday.in/rss/1206577",
+        "source": "India Today",
+        "bias": "Center"
+    },
+    {
+        "url": "http://feeds.news24.com/articles/news24/TopStories/rss",
+        "source": "News24",
+        "bias": "Right"
+    },
+    {
+        "url": "http://feeds.news24.com/articles/news24/World/rss",
+        "source": "News24",
+        "bias": "Left"
+    },
+    {
+        "url": "http://feeds.24.com/articles/sport/featured/topstories/rss",
+        "source": "News24",
+        "bias": "Right"
+    },
+    {
+        "url": "http://feeds.news24.com/articles/fin24/news/rss",
+        "source": "News24",
+        "bias": "Right"
+    },
+    {
+        "url": "http://feeds.news24.com/articles/news24/opinions/rss",
+        "source": "News24",
+        "bias": "Right"
+    },
+    {
+        "url": "https://ddnewsportal.com/rss/latest-posts",
+        "source": "DD News",
+        "bias": "Right"
+    },
+    {
+        "url": "https://ddnewsportal.com/rss/category/article",
+        "source": "DD News",
+        "bias": "Center"
+    },
+    {
+        "url": "https://ddnewsportal.com/rss/category/business",
+        "source": "DD News",
+        "bias": "Right"
+    },
+    {
+        "url": "https://ddnewsportal.com/rss/category/National",
+        "source": "DD News",
+        "bias": "Left"
+    },
+    {
+        "url": "https://ddnewsportal.com/rss/category/sports",
+        "source": "DD News",
+        "bias": "Center"
+    },
+    {
+        "url": "https://ddnewsportal.com/rss/category/Features",
+        "source": "DD News",
+        "bias": "Right"
+    },
+    {
+        "url": "https://www.thequint.com/feed",
+        "source": "The Quint",
+        "bias": "Center"
+    },
+    {
+        "url": "https://www.firstpost.com/commonfeeds/v1/mfp/rss/india.xml",
+        "source": "Firstpost",
+        "bias": "Left"
+    },
+    {
+        "url": "https://www.firstpost.com/commonfeeds/v1/mfp/rss/world.xml",
+        "source": "Firstpost",
+        "bias": "Right"
+    },
+    {
+        "url": "https://www.firstpost.com/commonfeeds/v1/mfp/rss/politics.xml",
+        "source": "Firstpost",
+        "bias": "Right"
+    },
+    {
+        "url": "https://www.firstpost.com/commonfeeds/v1/mfp/rss/business.xml",
+        "source": "Firstpost",
+        "bias": "Lean Left"
+    },
+    {
+        "url": "https://www.firstpost.com/commonfeeds/v1/mfp/rss/sports.xml",
+        "source": "Firstpost",
+        "bias": "Center"
+    },
+    {
+        "url": "https://www.firstpost.com/commonfeeds/v1/mfp/rss/opinion.xml",
+        "source": "Firstpost",
+        "bias": "Center"
+    },
+    {
+        "url": "https://www.firstpost.com/commonfeeds/v1/mfp/rss/explainers.xml",
+        "source": "Firstpost",
+        "bias": "Left"
+    },
+    {
+        "url": "https://www.cnbctv18.com/commonfeeds/v1/cne/rss/latest.xml",
+        "source": "CNBC TV18",
+        "bias": "Right"
+    },
+    {
+        "url": "https://www.cnbctv18.com/commonfeeds/v1/cne/rss/india.xml",
+        "source": "CNBC TV18",
+        "bias": "Right"
+    },
+    {
+        "url": "https://www.cnbctv18.com/commonfeeds/v1/cne/rss/economy.xml",
+        "source": "CNBC TV18",
+        "bias": "Left"
+    },
+    {
+        "url": "https://www.cnbctv18.com/commonfeeds/v1/cne/rss/market.xml",
+        "source": "CNBC TV18",
+        "bias": "Left"
+    },
+    {
+        "url": "https://www.cnbctv18.com/commonfeeds/v1/cne/rss/business.xml",
+        "source": "CNBC TV18",
+        "bias": "Right"
+    },
+    {
+        "url": "https://www.cnbctv18.com/commonfeeds/v1/cne/rss/sports.xml",
+        "source": "CNBC TV18",
+        "bias": "Left"
+    },
+    {
+        "url": "https://www.cnbctv18.com/commonfeeds/v1/cne/rss/politics.xml",
+        "source": "CNBC TV18",
+        "bias": "Left"
+    },
+    {
+        "url": "https://www.cnbctv18.com/commonfeeds/v1/cne/rss/world.xml",
+        "source": "CNBC TV18",
+        "bias": "Right"
+    },
+]
